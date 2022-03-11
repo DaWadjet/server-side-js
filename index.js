c2be610fdd1f84b46523d60bb762770ce91e5706
Czeglédi Levente <czegledi.levente@wadjet.hu>
2022-03-11 18:46:50 +0100

    added html pages and formatting by prettier

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const dotenv = require('dotenv').config()
-const port = process.env.PORT || 3000
+const express = require("express");
+const dotenv = require("dotenv").config();
+const port = process.env.PORT || 3000;
 
-const app = express()
+const app = express();
 
-app.use(express.static('static'))
+app.use(express.static("static"));
 
 const server = app.listen(port, function () {
-    console.log(`Express listening on http://localhost:${port}/`)
-})
+  console.log(`Express listening on http://localhost:${port}/`);
+});
